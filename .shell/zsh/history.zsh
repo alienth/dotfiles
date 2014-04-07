73e73c7fe25da72020f4d71dde4e8e4a9804b5d4
Jason Harvey <alienth@reddit.com>
2014-04-06 23:41:10 -0800

    history tunings

--- .shell/zsh/history.zsh
+++ .shell/zsh/history.zsh
@@ -5,3 +5,8 @@ SAVEHIST=9999999999
 HOSTNAME=`hostname -s || echo unknown`
 HISTFILE=~/.zsh_history/$HOSTNAME
 
+
+setopt histignoredups sharehistory extendedhistory
+setopt histverify
+setopt histignorespace
+

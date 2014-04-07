fcac51610b9ec9a1b578af44a053555dcf56cd59
Jason Harvey <alienth@reddit.com>
2014-04-06 23:48:24 -0800

    cd function

---
+++ .shell/zsh/cd.zsh
@@ -0,0 +1,24 @@
+cd () {
+
+    builtin cd "$*"
+    if [ $? -ne 0 ]; then 
+        if [ ! -x "$1" ] && [ -d "$1" ]; then
+                echo -n "Cannot access dir, become root? ";
+                read foo
+                if [[ $foo = "y" ]] || [[ $foo = "Y" ]]; then
+                        sudo zsh 
+                        return
+                else
+                        builtin cd "$*"
+                        return
+                fi
+        fi
+    else
+                ls --color=auto
+    fi
+}
+
+cdd () {
+    builtin cd "$*"
+}
+

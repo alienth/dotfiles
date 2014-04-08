3304623b6a6fadd18a77382fe095c3757bdc1ba7
Jason Harvey <alienth@gmail.com>
2014-04-08 01:58:09 -0700

    Handle no match better.

--- .shell/zsh/ssh_agent.zsh
+++ .shell/zsh/ssh_agent.zsh
@@ -2,11 +2,15 @@
 export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
 
 function find_ssh_auth_sock() {
+  emulate -L zsh
+  setopt nonomatch
+
+  sockets=(/tmp/keyring-*/ssh)
   # Check if lsof exists
-  if command -v lsof &> /dev/null; then
+  if command -v lsof &> /dev/null && (($#sockets)); then
 
     # Search for keyring sockets and use them if they're open
-    for file in /tmp/keyring-*/ssh; do
+    for file in sockets; do
       if lsof $file &> /dev/null; then
         export SSH_AUTH_SOCK=$file
         return

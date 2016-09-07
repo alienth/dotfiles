217ccea3308d82ece5d6ba59095eddd4ba627589
Jason Harvey <alienth@gmail.com>
2016-09-07 12:55:03 -0800

    Add aliases for systemd service mgmt.

--- .shell/zsh/aliases.zsh
+++ .shell/zsh/aliases.zsh
@@ -14,3 +14,26 @@ alias dud='du -xch . --max-depth=1 | sort -h'
 alias sdud='sudo du -xch . --max-depth=1 | sort -h'
 
 alias info='info --vi-keys'
+
+which systemd &>/dev/null
+if [[ $? -eq 0 ]]; then
+    which start &>/dev/null
+    if [[ $? -eq 1 ]]; then
+        alias start='systemctl start'
+    fi
+
+    which stop &>/dev/null
+    if [[ $? -eq 1 ]]; then
+        alias stop='systemctl stop'
+    fi
+
+    which restart &>/dev/null
+    if [[ $? -eq 1 ]]; then
+        alias restart='systemctl restart'
+    fi
+
+    which status &>/dev/null
+    if [[ $? -eq 1 ]]; then
+        alias status='systemctl status'
+    fi
+fi

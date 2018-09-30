3249fe1d21d5e3d18e4ff49ffdf18fe9e8680859
Jason Harvey <alienth@gmail.com>
2018-09-30 03:23:17 -0800

    Correct command-not-found setup.

--- .shell/zsh/command-not-found.zsh
+++ .shell/zsh/command-not-found.zsh
@@ -1,5 +1,5 @@
 if [[ -x /usr/lib/command-not-found ]] ; then
   function command_not_found_handler() {
-    /usr/lib/command-not-found --no-failure-msg -- $1
+    /usr/lib/command-not-found -- ${1+"$1"} && :
   }
 fi

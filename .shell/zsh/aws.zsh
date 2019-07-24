16a94de5da4adffac6380dc21a7b2b4d0647e3f0
Jason Harvey <alienth@gmail.com>
2019-07-24 13:34:28 -0800

    Add get_instance_public_ip.

--- .shell/zsh/aws.zsh
+++ .shell/zsh/aws.zsh
@@ -1,1 +1,2 @@
 alias get_instance_ip="aws ec2 describe-instances --query 'Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddress' --output text --instance-ids"
+alias get_instance_public_ip="aws ec2 describe-instances --query 'Reservations[*].Instances[*].NetworkInterfaces[*].Association.PublicIp' --output text --instance-ids"

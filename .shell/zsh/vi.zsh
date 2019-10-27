1d49dacfa471052119b2e709992806cce99a6a08
Jason Harvey <alienth@gmail.com>
2019-10-27 15:42:44 -0800

    lint.

--- .shell/zsh/vi.zsh
+++ .shell/zsh/vi.zsh
@@ -1,9 +1,9 @@
 export MANPAGER="/bin/sh -c 'col -b | vim -c '\''set ft=man nomod nolist signcolumn=no nonu'\'' -'"
 
 if [ -x $(which vim) ]; then
-	VI=$(which vim)
+  VI=$(which vim)
 else
-	VI=$(which vi)
+  VI=$(which vi)
 fi
 
 #complete -c vipath
@@ -11,26 +11,25 @@ vipath () {
         $VI $(which $1)
 }
 
-
 vi () {
-	if [ ! -d "$1" ] && [ ! -f "$1" ]; then
-		$VI "$@";
-		return
-	fi
+  if [ ! -d "$1" ] && [ ! -f "$1" ]; then
+    $VI "$@";
+    return
+  fi
 
-	if [[ ! -w "$1" && -f "$1" ]]; then
-                echo -n "Not writable. Become root?"
-                local foo
-                read -q foo
-                echo
-                if [[ $foo = "y" ]]; then
-                        sudo $VI "$@";
-                        return
-                else
-        		$VI "$@";
-                        return
-                fi
-	fi
-        $VI "$@";
+  if [[ ! -w "$1" && -f "$1" ]]; then
+    echo -n "Not writable. Become root?"
+    local foo
+    read -q foo
+    echo
+    if [[ $foo = "y" ]]; then
+      sudo $VI "$@";
+      return
+    else
+      $VI "$@";
+      return
+    fi
+  fi
+  $VI "$@";
 }
 

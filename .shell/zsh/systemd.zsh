bf83d76a5c279e5e3985db06ad1e344702a56dc6
Jason Harvey <alienth@gmail.com>
2019-10-28 22:53:02 -0800

    Enable wrapping on SYSTEMD_LESS.

--- .shell/zsh/systemd.zsh
+++ .shell/zsh/systemd.zsh
@@ -4,3 +4,7 @@ if [[ -x /bin/systemctl ]]; then
   alias restart='sudo systemctl restart'
   alias status='sudo systemctl status'
 fi
+
+# `less` options to use on pager. This is default other than the re-enabling of
+# wrapping.
+export SYSTEMD_LESS=FRXMK

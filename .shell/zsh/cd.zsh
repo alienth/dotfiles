a70eaf3f02dbd6ab02814e4f1490292b384a2617
Jason Harvey <alienth@gmail.com>
2019-11-05 14:33:25 -0900

    Handle cd flags.

--- .shell/zsh/cd.zsh
+++ .shell/zsh/cd.zsh
@@ -1,14 +1,15 @@
 cd () {
+  local DIR=${@[-1]}
   local STATUS
-  builtin cd "$*"
+  builtin cd $*
   STATUS=$?
   if [[ $STATUS -ne 0 ]]; then
-    if [[ ! -x "$1" ]] && [[ -d "$1" ]]; then
+    if [[ ! -x "$DIR" ]] && [[ -d "$DIR" ]]; then
       echo "Cannot access dir. sudo? ";
       local INPUT
       read -qs INPUT
       if [[ $INPUT = "y" ]]; then
-        sudo /bin/sh -c 'cd "'$*'"; exec zsh'
+        sudo /bin/sh -c "cd $*; exec zsh"
         return
       else
         return $STATUS

e0e066930bd586feb239f7a8bc050b31366e48b9
Jason Harvey <alienth@gmail.com>
2020-07-05 21:09:21 -0800

    zsh: Cleanup history settings.

--- .shell/zsh/history.zsh
+++ .shell/zsh/history.zsh
@@ -1,7 +1,7 @@
-# Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
-HISTSIZE=9999999999
-HISTFILESIZE=9999999999
-SAVEHIST=9999999999
+# Keep all lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=10000000000
+# Save all lines to the history file - the rest will be persisted via histdb.
+SAVEHIST=10000000000
 HISTFILE=~/.zsh_history/$HOST
 
 

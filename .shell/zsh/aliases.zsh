d77ea3fdad4437dd7820e7af9d2407db8ab2e6a8
Jason Harvey <alienth@gmail.com>
2020-08-08 02:14:22 -0800

    shell: Add suspendlock alias.

--- .shell/zsh/aliases.zsh
+++ .shell/zsh/aliases.zsh
@@ -20,3 +20,11 @@ alias info='info --vi-keys'
 if [[ -x /usr/bin/htop ]]; then
   alias top='htop'
 fi
+
+alias suspendlock='mate-screensaver-command -l; dbus-send \
+  --system \
+  --print-reply \
+  --dest=org.freedesktop.login1 \
+  /org/freedesktop/login1 \
+  org.freedesktop.login1.Manager.Suspend \
+  boolean:true'
